f744c67ddbb62a85317498d1a68dfec16ba532db
Matthieu MAXIT <matthieu.maxit@etu.unice.fr>
2017-03-14 23:31:08 +0100

    product

--- src/main/controller/ReceptionController.java
+++ src/main/controller/ReceptionController.java
@@ -11,9 +11,9 @@ public class ReceptionController {
 	public Text openning, phone, email, floor, clue, desc, commentary;
 
 	public void init(Information info) {
-		this.openning.textProperty().bind(Bindings.concat("Du ", info.getOpenDay(), " au ", info.getCloseDay(), " de ", info.getOpenHour(), info.getOpenMin(), " à ", info.getCloseHour(), info.getCloseMin()));
-		this.phone.textProperty().bind(Bindings.concat("Tél.: +33 78 27 39 ", info.getPhone()));
-		this.email.textProperty().bind(Bindings.concat("email: ", info.getEmail(), "@capsophia.fr"));
+		this.openning.textProperty().bind(Bindings.concat("Du ", info.openDayProperty(), " au ", info.closeDayProperty(), " de ", info.openHourProperty(), info.openMinProperty(), " à ", info.closeHourProperty(), info.closeMinProperty()));
+		this.phone.textProperty().bind(Bindings.concat("Tél.: +33 78 27 39 ", info.phoneProperty()));
+		this.email.textProperty().bind(Bindings.concat("email: ", info.emailProperty(), "@capsophia.fr"));
 		this.floor.textProperty().bind(Bindings.concat("Niveau: ", info.floorProperty()));
 		this.clue.textProperty().bind(info.clueProperty());
 		this.desc.textProperty().bind(info.descProperty());
