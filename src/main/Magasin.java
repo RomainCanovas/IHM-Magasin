998b7573ba9874d3e9b63ec7f5e8bdcbca3d047a
Romain Canovas <romain.canovas@etu.unice.fr>
2017-03-15 15:01:10 +0100

    CSS

--- src/main/Magasin.java
+++ src/main/Magasin.java
@@ -9,7 +9,9 @@ public class Magasin extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("controller/Magasin.fxml"));
-        primaryStage.setScene(new Scene(root));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add("controller/view/theme.css");
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
